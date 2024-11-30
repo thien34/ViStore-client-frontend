5ca2790497b620b1ee6f5815f5d24f5461037579
nguyenkien <nguyenkienynk@gmail.com>
2024-11-30 14:11:05 +0700

    #1 - update service info user

--- interface/customer.interface.ts
+++ interface/customer.interface.ts
@@ -1,3 +1,23 @@
+export interface CustomerFullRequest {
+    id: number
+
+    customerGuid: string
+
+    email: string
+
+    firstName: string
+
+    lastName: string
+
+    gender: number
+
+    dateOfBirth: Date
+
+    customerRoles: number[]
+
+    active: boolean
+}
+
 export interface CustomerUpdateRequest {
     firstName: string
     lastName: string
--- service/customer.service.ts
+++ service/customer.service.ts
@@ -1,11 +1,25 @@
-import { CustomerUpdateRequest } from '@/interface/customer.interface'
+import { CustomerFullRequest, CustomerUpdateRequest } from '@/interface/customer.interface'
 import http from '@/lib/http'
 
 class CustomerService {
     private basePath = '/api/admin/customers'
+
+    async getAll() {
+        return await http.get<CustomerFullRequest[]>(`${this.basePath}`)
+    }
+    async getById(id: number) {
+        return await http.get<CustomerFullRequest>(`${this.basePath}/${id}`)
+    }
+    async create(customer: CustomerFullRequest) {
+        const response = await http.post(`${this.basePath}`, customer)
+        return response.payload
+    }
     async update(id: number, customer: Omit<CustomerUpdateRequest, 'id'>) {
         const response = await http.put(`${this.basePath}/profile-info/${id}`, customer)
         return response.payload
+    }
+    async delete(id: number): Promise<void> {
+        await http.delete<void>(`${this.basePath}/${id}`)
     }
 }
 const customerService = new CustomerService()
